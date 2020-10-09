89cd00542cf704a68ffd59100e22226ea1a7d8b8
Sergey <homevictim19@gmail.com>
2020-10-10 02:43:29 +0300

    Updates

---
+++ precache-manifest.5be09f5bf96249c91b767377717a1ac5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38394562846c5828907aeed91d6daf88",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "729559c37e5334a41b72",
+    "url": "/weather-app/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "c3c436e7bea6838627e2",
+    "url": "/weather-app/static/css/main.0f1b9c31.chunk.css"
+  },
+  {
+    "revision": "729559c37e5334a41b72",
+    "url": "/weather-app/static/js/2.abbae701.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.abbae701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c436e7bea6838627e2",
+    "url": "/weather-app/static/js/main.8ba083b3.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "b674700cc8a7656e5b9affb564bf73a7",
+    "url": "/weather-app/static/media/002-wind-sign.b674700c.svg"
+  },
+  {
+    "revision": "5f02df50e1bb247143d6ba8c71a923f2",
+    "url": "/weather-app/static/media/004-umbrella.5f02df50.svg"
+  },
+  {
+    "revision": "a3ae7e9e9829fd0a064bed6175f11466",
+    "url": "/weather-app/static/media/006-thunderstorm.a3ae7e9e.svg"
+  },
+  {
+    "revision": "474a832baf77350e99eedb1679d48795",
+    "url": "/weather-app/static/media/007-thermometer-4.474a832b.svg"
+  },
+  {
+    "revision": "fa3a4662bf87f7cd305bd8bdaa939cd6",
+    "url": "/weather-app/static/media/013-sunny.fa3a4662.svg"
+  },
+  {
+    "revision": "d416e972ff7be652e4109eeee61af7a4",
+    "url": "/weather-app/static/media/016-snowing-1.d416e972.svg"
+  },
+  {
+    "revision": "f334d20a8d68bc26143d410683ac0deb",
+    "url": "/weather-app/static/media/019-raining.f334d20a.svg"
+  },
+  {
+    "revision": "66e73bdba6dd8ea34dfaf52ed61e3e8c",
+    "url": "/weather-app/static/media/023-rain-4.66e73bdb.svg"
+  },
+  {
+    "revision": "a510d1e43bc3b884ba4a6efb9eac85df",
+    "url": "/weather-app/static/media/031-cloudy.a510d1e4.svg"
+  },
+  {
+    "revision": "e12bfaaf348be2aa388e3e5972857db0",
+    "url": "/weather-app/static/media/035-cloud.e12bfaaf.svg"
+  },
+  {
+    "revision": "2cc4d6f47ae10317c333b7fa359e3aba",
+    "url": "/weather-app/static/media/Montserrat-Bold.2cc4d6f4.woff"
+  },
+  {
+    "revision": "7daa52924301182e215b5065bac34532",
+    "url": "/weather-app/static/media/Montserrat-Bold.7daa5292.woff2"
+  },
+  {
+    "revision": "684a237c49b05587d61c5e06fb227dd6",
+    "url": "/weather-app/static/media/Montserrat-Regular.684a237c.woff2"
+  },
+  {
+    "revision": "f554010862ffc82142d32458aec4b9e0",
+    "url": "/weather-app/static/media/Montserrat-Regular.f5540108.woff"
+  },
+  {
+    "revision": "b7db51b49d89f4ed45e1296c998f4e74",
+    "url": "/weather-app/static/media/bg.b7db51b4.jpg"
+  },
+  {
+    "revision": "4eeb5a23f5f399938e7c96374ae5f1a2",
+    "url": "/weather-app/static/media/search.4eeb5a23.svg"
+  }
+]);
